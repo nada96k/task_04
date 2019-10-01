3d126ffe21f38cacc8a50ad4d96b3fd360b8c23a
Your Namenada96k <nadawe_96@hotmail.com>
2019-10-01 19:14:28 +0300

    Restaurants added successfully

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	opening_time = models.CharField(max_length=100)
+	closing_time = models.CharField(max_length=100)
+
+
+	def __str__(self):
+		return self.name
